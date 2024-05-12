e0c6c68571d3c05ddb03360fcd52b24ea51b2e24
leon-dream1~ <nahidulislam019045@gmail.com>
2024-05-12 14:47:09 +0600

    Delete route added

--- index.js
+++ index.js
@@ -56,15 +56,45 @@ async function run() {
 
     app.post("/booking", async (req, res) => {
       const bookingData = req.body;
+      // console.log(req.body);
       const id = req.query.id;
 
       const result = await bookingCollection.insertOne(bookingData);
+      // console.log("Bboking", result);
 
       if (result.acknowledged) {
         const filter = { _id: new ObjectId(req.query.id) };
         const updateDocument = {
           $set: {
             availability: false,
+          },
+        };
+        const updatedResult = await roomCollection.updateOne(
+          filter,
+          updateDocument
+        );
+        // console.log("updated",updatedResult);
+        res.send(updatedResult);
+      }
+    });
+
+    app.get("/booking/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const result = await bookingCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.delete("/booking/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: id };
+      const result = await bookingCollection.deleteOne(query);
+
+      if (result.deletedCount) {
+        const filter = { _id: new ObjectId(req.params.id) };
+        const updateDocument = {
+          $set: {
+            availability: true,
           },
         };
         const updatedResult = await roomCollection.updateOne(
